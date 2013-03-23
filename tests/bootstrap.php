2b3cf54664f555495c2d26622c8fc2b60df24a92
kphoen <geek63@gmail.com>
2013-03-23 12:11:02 +0000

    Chg: added dev requirements in the composer.json file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,10 @@
 <?php
 
-if (file_exists($file = __DIR__.'/../autoload.php')) {
-    require_once $file;
+if (file_exists($file = __DIR__.'/../vendor/autoload.php')) {
+    $loader = include $file;
+    $loader->add('SmsSender\Tests', __DIR__);
+} else if (file_exists($file = __DIR__.'/../autoload.php')) {
+    include $file;
 } elseif (file_exists($file = __DIR__.'/../autoload.php.dist')) {
-    require_once $file;
+    include $file;
 }
