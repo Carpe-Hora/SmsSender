4db9985062f2f54d6c28b7a8406d7e75a75921d5
Kévin Gomez <geek63@gmail.com>
2013-09-02 22:45:16 +0100

    Add: dummy provider, to be used in development environments for instance

---
+++ src/SmsSender/Provider/DummyProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of the SmsSender package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace SmsSender\Provider;
+
+use SmsSender\Provider\ProviderInterface;
+use SmsSender\Result\ResultInterface;
+
+/**
+ * @author Kévin Gomez <kevin_gomez@carpe-hora.com>
+ */
+class DummyProvider implements ProviderInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function send($recipient, $body, $originator = '')
+    {
+        return array(
+            'id'         => uniqid(),
+            'recipient'  => $recipient,
+            'body'       => $body,
+            'originator' => $originator,
+            'status'     => ResultInterface::STATUS_SENT,
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'dummy';
+    }
+}
+
+// vim: set softtabstop=4 tabstop=4 shiftwidth=4 autoindent:
